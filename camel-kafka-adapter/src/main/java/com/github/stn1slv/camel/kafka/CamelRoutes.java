c0907b0083c66e6d6281d9db3c61b0429f58c151
stn1slv <devyatov@gmail.com>
2021-09-17 10:54:45 +0300

    Infra added

--- camel-kafka-adapter/src/main/java/com/github/stn1slv/camel/kafka/CamelRoutes.java
+++ camel-kafka-adapter/src/main/java/com/github/stn1slv/camel/kafka/CamelRoutes.java
@@ -8,6 +8,6 @@ public class CamelRoutes extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("kafka:output?brokers=kafka:9092").log("${body}");
+        from("kafka:output").log("${body}");
     }
 }
